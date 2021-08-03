45288b7dbd372b00df60e167f5c765320838fd65
heo-min0 <humanzxc@naver.com>
2021-08-03 11:27:42 +0900

    lombok 사용

--- src/main/java/org/springframework/samples/petclinic/owner/Page.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Page.java
@@ -1,5 +1,14 @@
 package org.springframework.samples.petclinic.owner;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Page {
 
     private String lastName;
@@ -10,63 +19,4 @@ public class Page {
     private int startPage;
     private int endPage;
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public int getCurrentPage() {
-        return currentPage;
-    }
-
-    public void setCurrentPage(int currentPage) {
-        this.currentPage = currentPage;
-    }
-
-    public int getTotalPage() {
-        return totalPage;
-    }
-
-    public void setTotalPage(int totalPage) {
-        this.totalPage = totalPage;
-    }
-
-    public int getStartPage() {
-        return startPage;
-    }
-
-    public void setStartPage(int startPage) {
-        this.startPage = startPage;
-    }
-
-    public int getEndPage() {
-        return endPage;
-    }
-
-    public void setEndPage(int endPage) {
-        this.endPage = endPage;
-    }
-
-    @Override
-    public String toString() {
-        return "Page{" +
-            "lastName='" + lastName + '\'' +
-            ", firstName='" + firstName + '\'' +
-            ", currentPage=" + currentPage +
-            ", totalPage=" + totalPage +
-            ", startPage=" + startPage +
-            ", endPage=" + endPage +
-            '}';
-    }
 }
